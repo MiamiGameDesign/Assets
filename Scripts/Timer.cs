ef428de490faebe2ba6ad74aa0cf31a5907be112
kleinjb25 <kleinjb@miamioh.edu>
2023-04-29 01:12:23 -0400

    added a simple timer script

---
+++ Scripts/Timer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject textBox;
+    float startTime, timeElapsed;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+    void Update() {
+        if (Time.timeScale != 0)
+            timeElapsed = (Time.time - startTime) * Time.timeScale;
+            textBox.GetComponent<Text>().text = "Time: " + (Mathf.Round(timeElapsed * 100f) / 100f) % 60;
+    }
+}
